1fe95401b10d78d936f6c6747e829e1b91526cac
Tiago mendes <tiago.mendes@readinessit.com>
2020-07-15 15:35:09 +0100

    -Showing the image of the person in the marker

---
+++ app/src/main/java/pt/ubi/eventtrackingapp/ImageHandler.java
@@ -0,0 +1,26 @@
+package pt.ubi.eventtrackingapp;
+
+import android.content.Context;
+
+import com.squareup.picasso.Cache;
+import com.squareup.picasso.Picasso;
+
+import java.util.concurrent.Executors;
+
+public class ImageHandler {
+
+    private static Picasso instance;
+
+    public static Picasso getSharedInstance(Context context)
+    {
+        if(instance == null)
+        {
+            instance = new Picasso.Builder(context).executor(Executors.newSingleThreadExecutor()).memoryCache(Cache.NONE).indicatorsEnabled(true).build();
+            return instance;
+        }
+        else
+        {
+            return instance;
+        }
+    }
+}
--- app/src/main/java/pt/ubi/eventtrackingapp/myClusterManagerRenderer.java
+++ app/src/main/java/pt/ubi/eventtrackingapp/myClusterManagerRenderer.java
@@ -2,7 +2,10 @@ package pt.ubi.eventtrackingapp;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
@@ -16,30 +19,33 @@ import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
 import com.google.maps.android.clustering.view.DefaultClusterRenderer;
 import com.google.maps.android.ui.IconGenerator;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 public class myClusterManagerRenderer extends DefaultClusterRenderer<MyClusterItem> {
 
-    private final IconGenerator iconGenerator;
-    private final ImageView imageView;
+
     private final int markerWidth;
     private final int markerHeight;
-
+    private final Context context;
+    private final ImageView imageView;
+    private final IconGenerator iconGenerator;
 
 
 
     public myClusterManagerRenderer(Context context, GoogleMap map, ClusterManager<MyClusterItem> clusterManager) {
         super(context, map, clusterManager);
+        this.context = context.getApplicationContext();
 
-        // initialize cluster item icon generator
-        iconGenerator = new IconGenerator(context.getApplicationContext());
         imageView = new ImageView(context.getApplicationContext());
+        iconGenerator = new IconGenerator(context.getApplicationContext());
+
+
         markerWidth = (int) context.getResources().getDimension(R.dimen.custom_marker_image);
         markerHeight = (int) context.getResources().getDimension(R.dimen.custom_marker_image);
-        imageView.setLayoutParams(new ViewGroup.LayoutParams(markerWidth, markerHeight));
         int padding = (int) context.getResources().getDimension(R.dimen.custom_marker_padding);
-        imageView.setPadding(padding, padding, padding, padding);
-        iconGenerator.setContentView(imageView);
+
 
     }
 
@@ -48,16 +54,7 @@ public class myClusterManagerRenderer extends DefaultClusterRenderer<MyClusterIt
 
     @Override
     protected void onBeforeClusterItemRendered(MyClusterItem item, MarkerOptions markerOptions) {
-        if(item.getIconPicture()!= null)
-            Picasso.get().load(item.getIconPicture()).fit().centerCrop().into(imageView);
-
-        Bitmap icon = iconGenerator.makeIcon();
-        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(icon)).title(item.getTitle());
-    }
-
-    @Override
-    protected boolean shouldRenderAsCluster(Cluster cluster) {
-        return false;
+        markerOptions.visible(false);
     }
 
     /**
@@ -70,5 +67,29 @@ public class myClusterManagerRenderer extends DefaultClusterRenderer<MyClusterIt
             marker.setPosition(clusterMarker.getPosition());
         }
     }
+
+
+    @Override
+    protected void onClusterItemRendered(MyClusterItem item, final Marker marker) {
+
+
+
+        ImageHandler.getSharedInstance(context).load(item.getIconPicture()).resize(100, 100).centerCrop().into(imageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+                Bitmap bitmap = drawable.getBitmap();
+                marker.setIcon(BitmapDescriptorFactory.fromBitmap(bitmap));
+                marker.setVisible(true);
+            }
+
+
+            @Override
+            public void onError(Exception e) {
+            }
+
+        });
+    }
+
 
 }

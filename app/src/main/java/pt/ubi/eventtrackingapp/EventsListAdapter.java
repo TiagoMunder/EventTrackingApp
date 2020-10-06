4dcc226d6b2b5cbdbcddc4daf94d31cae78491d5
Tiago mendes <tiago.mendes@readinessit.com>
2020-10-06 15:23:03 +0100

    -add delete Image -improve list events visually

--- app/src/main/java/pt/ubi/eventtrackingapp/EventsListAdapter.java
+++ app/src/main/java/pt/ubi/eventtrackingapp/EventsListAdapter.java
@@ -23,9 +23,8 @@ public class EventsListAdapter extends ArrayAdapter<Event> {
 
 
     private static class ViewHolder {
-        TextView name;
-        TextView owner;
-        TextView description;
+        TextView name, owner, description, eventDate, location;
+
     }
 
     public EventsListAdapter(Context context, int resource, ArrayList<Event> objects) {
@@ -40,7 +39,11 @@ public class EventsListAdapter extends ArrayAdapter<Event> {
         String name = getItem(position).getName();
         String owner = getItem(position).getOwner();
         String description = getItem(position).getDescription();
-
+        String event_date = getItem(position).getDate();
+        String city = getItem(position).getCity() ;
+        String country = getItem(position).getCountry();
+        String street = getItem(position).getStreet() ;
+        String location = street + ','+ city + ',' + country;
         final View result;
         ViewHolder holder;
         if(convertView == null) {
@@ -48,9 +51,11 @@ public class EventsListAdapter extends ArrayAdapter<Event> {
             convertView = inflater.inflate(mResource, parent,false);
 
             holder= new ViewHolder();
-            holder.name = (TextView) convertView.findViewById(R.id.name);
+            holder.name = (TextView) convertView.findViewById(R.id.event_name);
             holder.owner = (TextView) convertView.findViewById(R.id.owner);
             holder.description = (TextView) convertView.findViewById(R.id.description);
+            holder.eventDate = (TextView) convertView.findViewById(R.id.date);
+            holder.location = (TextView) convertView.findViewById(R.id.location);
             result = convertView;
             convertView.setTag(holder);
         } else {
@@ -67,7 +72,8 @@ public class EventsListAdapter extends ArrayAdapter<Event> {
         holder.name.setText(name);
         holder.owner.setText(owner);
         holder.description.setText(description);
-
+        holder.eventDate.setText(event_date);
+        holder.location.setText(location);
         return convertView;
     }
 }
